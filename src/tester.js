2290a56046045f4f71e0bdf92d9d6c5ab2a16557
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-03-28 11:56:21 +0200

    Add a very basic tester

---
+++ src/tester.js
@@ -0,0 +1,5 @@
+﻿var sk = this.H.ScoreKeeper,
+    fed = new sk.Party('Fed Team').addMember(new sk.Individual('Dan', 'Hintea')),
+    rafa = new sk.Party('Rafa Team').addMember(new sk.Individual('Gicu', 'Traian')),
+    clash = new sk.Clash([fed, rafa]),
+    projector = new sk.Projector(clash);
