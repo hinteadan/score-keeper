95a627694d8289263d92503b38e279295beac93d
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-04-03 15:45:28 +0300

    Add grunt

---
+++ src/Gruntfile.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    
+    grunt.initConfig({
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            'build/*',
+                            '!build/.git*'
+                        ]
+                    }
+                ]
+            },
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '{,*/}*.js'
+            ]
+        }
+    });
+
+    grunt.registerTask('default', [
+        'jshint'
+    ]);
+};
