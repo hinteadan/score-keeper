a8bb1f0a740907e1469725fbfb7de324cee6714a
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-06-12 15:34:48 +0300

    Add factory option for details object on model builder

--- src/modelBuilder.js
+++ src/modelBuilder.js
@@ -5,9 +5,9 @@
         return new sk.Individual(dto.firstName, dto.lastName);
     };
 
-    sk.Party.revive = function (dto, individualsPool) {
+    sk.Party.revive = function (dto, individualsPool, detailsFactory) {
         /// <param name='individualsPool' optional='true' />
-        return new sk.Party(dto.name, dto.details)
+        return new sk.Party(dto.name, detailsFactory ? detailsFactory(dto.details) : dto.details)
             .addMembers(map(dto.individuals, function (individualDto) {
                 if (!individualsPool) {
                     return sk.Individual.revive(individualDto);
@@ -17,22 +17,23 @@
             }));
     };
 
-    sk.Point.revive = function (dto, partyPool) {
+    sk.Point.revive = function (dto, partyPool, detailsFactory) {
         /// <param name='partyPool' optional='true' />
         return new sk.Point(
             partyPool ? find(partyPool, function (party) { return party.name === dto.party.name; }) : sk.Party.revive(dto),
-            dto.details).setTimestamp(dto.timestamp);
+            detailsFactory ? detailsFactory(dto.details) : dto.details).setTimestamp(dto.timestamp);
     };
 
-    sk.Clash.revive = function (dto, partyPool) {
+    sk.Clash.revive = function (dto, partyPool, detailsFactory) {
         /// <param name='partyPool' optional='true' />
         var parties = map(dto.parties, function (partyDto) {
             return partyPool ? find(partyPool, function (p) { return p.name === partyDto.name; }) : sk.Party.revive(partyDto);
         });
 
-        return sk.Clash(parties, dto.details).addPoints(map(dto.points, function (pointDto) {
-            return sk.Point.revive(pointDto, parties);
-        }));
+        return sk.Clash(parties, detailsFactory ? detailsFactory(dto.details) : dto.details)
+            .addPoints(map(dto.points, function (pointDto) {
+                return sk.Point.revive(pointDto, parties);
+            }));
     };
 
 }).call(this, this.H.ScoreKeeper, this.H.JsUtils.map, this.H.JsUtils.find);
